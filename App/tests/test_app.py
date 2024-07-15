5d8455bce08507553bf226e2a031804634369832
Quan Afoon <quanafoon@gmail.com>
2024-07-15 19:52:22 +0000

    test2

---
+++ App/tests/test_app.py
@@ -0,0 +1,21 @@
+import os, tempfile, pytest, logging, unittest
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from App.main import create_app
+from App.database import db, create_db
+from App.models import User
+from App.controllers import (
+    create_user,
+    get_all_users_json,
+    login,
+    get_user,
+    get_user_by_username,
+    update_user
+)
+
+
+LOGGER = logging.getLogger(__name__)
+
+'''
+   Unit Tests
+'''
