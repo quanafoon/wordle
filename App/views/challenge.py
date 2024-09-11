27c70f2591ff7fef34ab3920092d725cc2b5d8ad
Quan Afoon <quanafoon@gmail.com>
2024-09-11 09:20:58 +0000

    coordination between players

--- App/views/challenge.py
+++ App/views/challenge.py
@@ -73,10 +73,10 @@ def cancel_challenge_route(code):
         return render_template("waiting.html", code=code)
 
 @challenge_views.route('/cancelUnload/<string:code>', methods=['POST'])
-def cancel_challenge2(code):
-    challengeID = get_challengeID(code)
-    if challengeID:
-        challenge = cancel_challenge(challengeID)
+def player_left(code):
+    challenge = removePlayer(code)
+    if challenge:
+        check(code)
     return '', 204  # No Content
 
 @challenge_views.route('/join', methods=['POST'])
